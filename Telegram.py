d4d82fe040667b826d943cad67b55356056c6b0a
FrancescoPivetti01 <francesco.pivetti@fermi.mo.it>
2019-05-23 17:41:04 +0200

    MQTT done

--- Telegram.py
+++ Telegram.py
@@ -14,7 +14,7 @@ keyboard = pzgram.create_inline(k)
 
 
 def start(chat):
-    global message, keyboard, listaFinale
+    global message, keyboard
     chat.send(message, reply_markup=keyboard)
 
 
@@ -22,43 +22,39 @@ def get_istant(chat):
     global keyboard, error_message
     if not listaFinale[0]:
         chat.send(error_message)
-        start(chat)
     else:
-        chat.send("Il valore di temperatura istantaneo è: ".format(listaFinale[0]))
-        start(chat)
+        chat.send("Il valore di temperatura istantaneo è: {}".format(listaFinale[0]))
+    start(chat)
 
 
 def get_min1(chat):
     global keyboard, error_message
     if not listaFinale[1]:
         chat.send(error_message)
-        start(chat)
     else:
-        chat.send("Il valore di temperatura dopo 1 minuto è: ".format(listaFinale[1]))
-        start(chat)
+        chat.send("Il valore di temperatura dopo 1 minuto è: {}".format(listaFinale[1]))
+    start(chat)
 
 
 def get_min10(chat):
     global keyboard, error_message
     if not listaFinale[2]:
         chat.send(error_message)
-        start(chat)
     else:
-        chat.send("Il valore di temperatura dopo 10 minuti è: ".format(listaFinale[2]))
-        start(chat)
+        chat.send("Il valore di temperatura dopo 10 minuti è: {}".format(listaFinale[2]))
+    start(chat)
 
 
 def get_h1(chat):
     global keyboard, error_message
     if not listaFinale[3]:
         chat.send(error_message)
-        start(chat)
     else:
-        chat.send("Il valore di temperatura dopo 1 ora è: ".format(listaFinale[3]))
-        start(chat)
+        chat.send("Il valore di temperatura dopo 1 ora è: {}".format(listaFinale[3]))
+    start(chat)
 
 
-commands = {"begin": start}
+commands = {"start": start}
 bot.set_commands(commands)
 
 queries = {"istantaneo": get_istant, "1 minuto": get_min1, "10 minuti": get_min10, "1 ora": get_h1}
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import threading
 import Telegram
 import MQTT
 
-thread1 = threading.Thread(target=Telegram.test)
-thread2 = threading.Thread(target=MQTT.test)
+thread1 = threading.Thread(target=Telegram.main)
+thread2 = threading.Thread(target=MQTT.main)
 thread1.start()
 thread2.start()
